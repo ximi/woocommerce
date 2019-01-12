442511d515536b40065757bd068dc64c77fbae4b
ximi <zeemidesign@gmail.com>
2019-01-12 23:28:58 +0200

    Remove html tag from plain text email
    Removes `<strong>` html tag from `woocommerce/templates/emails/plain/customer-new-account.php` plain text email template

--- templates/emails/plain/customer-new-account.php
+++ templates/emails/plain/customer-new-account.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Customer new account email
- *
- * This template can be overridden by copying it to yourtheme/woocommerce/emails/plain/customer-new-account.php.
+ *woocommerce
+ * This template can be overridden by copying it to yourtheme//emails/plain/customer-new-account.php.
  *
  * HOWEVER, on occasion WooCommerce will need to update template files and you
  * (the theme developer) will need to copy the new files to your theme to
@@ -24,7 +24,7 @@ echo '= ' . esc_html( $email_heading ) . " =\n\n";
 /* translators: %s Customer first name */
 echo sprintf( esc_html__( 'Hi %s,', 'woocommerce' ), esc_html( $user_login ) ) . "\n\n";
 /* translators: %1$s: Site title, %2$s: Username, %3$s: My account link */
-echo sprintf( esc_html__( 'Thanks for creating an account on %1$s. Your username is %2$s. You can access your account area to view orders, change your password, and more at: %3$s', 'woocommerce' ), esc_html( $blogname ), '<strong>' . esc_html( $user_login ) . '</strong>', esc_html( wc_get_page_permalink( 'myaccount' ) ) ) . "\n\n";
+echo sprintf( esc_html__( 'Thanks for creating an account on %1$s. Your username is %2$s. You can access your account area to view orders, change your password, and more at: %3$s', 'woocommerce' ), esc_html( $blogname ), esc_html( $user_login ), esc_html( wc_get_page_permalink( 'myaccount' ) ) ) . "\n\n";
 
 if ( 'yes' === get_option( 'woocommerce_registration_generate_password' ) && $password_generated ) {
 	/* translators: %s Auto generated password */
